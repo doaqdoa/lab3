d79fe2395a33e8b98efbf08cf4130509ccb3a3a2
doaqdoa <doaqdoa@hotmail.com>
2018-09-26 03:22:39 +0700

    CSS Class

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import CharacterCard from './CharacterCard';
+
 import './App.css';
+const word = "Hello";
+
 class App extends Component {
  render() {
  return (
- <div className="App">
- Hello {"World"}
- </div>
+    <div>
+    <CharacterCard value="h"/>
+    <CharacterCard value="i"/>
+    {
+     Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+    }
+
+    </div>
+   
  );
  }
 }
---
+++ card-game/src/CharacterCard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+export default 
+
+class CharacterCard extends Component {
+    render(){
+    return (
+    <div>
+    
+    {this.props.value}
+            
+          {this.props.card}
+          </div>
+    )
+    }
+   }
+
+   
+   
+   
