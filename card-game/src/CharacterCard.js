2edcb96809479d531f01c0c7789bc03e96053192
doaqdoa <doaqdoa@hotmail.com>
2018-09-27 04:06:50 +0700

    Fix.Handler as Props

--- card-game/src/CharacterCard.js
+++ card-game/src/CharacterCard.js
@@ -8,11 +8,18 @@ export default class CharacterCard extends Component {
     }
     }
    
+    //activate = () => {
+    //if(!this.state.active){this.setState({active: true})
     activate = () => {
-    if(!this.state.active){this.setState({active: true})
-    }
-    }
-    render(){
+        if(!this.state.active){
+        this.props.activationHandler(this.props.value)
+        this.setState({active: true})
+        }
+       }
+       
+    
+    
+    render(){ 
     let className = `card ${this.state.active ? 'activeCard': ''}`
     return (
     <div className={className} onClick={this.activate}>
