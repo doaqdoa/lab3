373ae0e6bd17e60f66796430a042b8db4d389b7a
doaqdoa <doaqdoa@hotmail.com>
2018-09-27 03:26:09 +0700

    Fix. Components & States

--- card-game/src/CharacterCard.js
+++ card-game/src/CharacterCard.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 
-
-
-
-export default class CharacterCard  extends Component {
+export default class CharacterCard extends Component {
+    constructor(props){
+    super(props)
+    this.state = {
+    active: false,
+    }
+    }
+   
+    activate = () => {
+    if(!this.state.active){this.setState({active: true})
+    }
+    }
     render(){
+    let className = `card ${this.state.active ? 'activeCard': ''}`
     return (
-
-        
-    <div>
-        
-        <span className="card">{this.props.value} </span>
-
-         
-          
-          </div>
+    <div className={className} onClick={this.activate}>
+    {this.props.value}
+    </div>
     )
     }
    }
-   
-   
